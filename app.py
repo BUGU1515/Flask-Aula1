8ca12f8de8a8630ed66de76b8f34bbeaabb08dad
BUGU1515 <juliotrindade@colegiopaulodetarso.com.br>
2025-11-05 15:55:52 +0000

    .

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "<h1>Hello world sss!!</h1>"
 
-@app.route('/bemvindo/<usuario>')
-def info():
-    modulo = "Flask"
-    aula = "1"
-    return f"<h1>Modulo: {modulo}, Aula: {aula}</h1>"
+def index():
+
+     return "Olá, Mundo !"
